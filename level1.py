10b155bd182273d97490a332bcd5e651df1c1fee
ShihYaoCheng <samsy318417@gmail.com>
2024-11-06 01:48:54 -0500

    level 1 candy drop

--- level1.py
+++ level1.py
@@ -1,4 +1,9 @@
+import random
 import tkinter as tk
+import pygame
+from sqlalchemy.sql.operators import truediv
+
+import cfg
 from PIL import Image, ImageTk
 
 def start_level1(root, level_selection_screen):
@@ -25,3 +30,68 @@ def start_level1(root, level_selection_screen):
         
     except Exception as e:
         print("Return icon not found:", e)
+
+    # init game
+    pygame.init()
+    screen = pygame.display.set_mode(cfg.SCREENSIZE)
+    pygame.display.set_caption("Halloween Game")
+    clock = pygame.time.Clock()
+
+    # player in middle bottom screen
+    player = pygame.Rect(
+        (cfg.SCREENSIZE[0] // 2 - cfg.PLAYER_SIZE[0] // 2,
+        cfg.SCREENSIZE[1] - cfg.PLAYER_SIZE[1] -10),
+        cfg.PLAYER_SIZE
+
+    )
+
+    candies = []
+
+    score = 0
+    font = pygame.font.SysFont(None, 36)
+
+
+    # game main loop
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+
+        # random candy drop
+        ranNum = random.randint(1,20)
+        if ranNum == 1 :
+            candy_x = random.randint(0, cfg.SCREENSIZE[0] - cfg.CANDY_SIZE[0])
+            new_candy = pygame.Rect(candy_x, 0, *cfg.CANDY_SIZE)
+            candies.append(new_candy)
+
+        for c in list(candies):
+            c.move_ip(0,cfg.CANDY_SPEED)
+            if c.colliderect(player): # touch player
+                candies.remove(c)
+                score += 1
+            elif c.top > cfg.SCREENSIZE[1]: # out of screen
+                candies.remove(c)
+
+
+
+
+
+        # screen rendering
+        screen.fill(cfg.BGCOLOR)
+        pygame.draw.rect(screen, (255,0,0), player)
+
+        # display candies
+        for c in candies:
+            pygame.draw.rect(screen, (0,0,255), c)
+
+        # display score
+        score_text = font.render(f"Score: {score}", True, (0, 0, 0))
+        screen.blit(score_text, (10, 10))
+
+        pygame.display.flip()
+        # update speed
+        clock.tick(cfg.FPS)
+
+    pygame.quit()
